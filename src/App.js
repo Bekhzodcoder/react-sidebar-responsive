20d347bbdc9ef76d75e519bb433fac1102821a27
Bekhzodcoder <bexzodcoder@gmail.com>
2022-10-26 09:20:52 +0300

    react sidebar admin panel

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
+import "./style/index.css";
+import Match from './components/Match/Match';
+
+function App() {
+	const [navVisible, showNavbar] = useState(true);
+
+	return (
+		<BrowserRouter >
+			<div className="App">
+				<Navbar visible={ navVisible } show={ showNavbar } />
+				<Routes>
+					{/* <Route path="/" element={<Navigate to="/dashboard" />} /> */}
+					<Route path='/dashboard' element={
+						<div className={!navVisible ? "page" : "page page-with-navbar"}>
+							<h1>Dashboard</h1>
+						</div>
+					} />
+					<Route path='/analytics' element={
+						<div className={!navVisible ? "page" : "page page-with-navbar"}>
+							<h1>Analystics</h1>
+						</div>
+					}/>
+					<Route path='/orders' element={
+						<div className={!navVisible ? "page" : "page page-with-navbar"}>
+							<h1>Orders</h1>
+						</div>
+					}/>
+					<Route path='/settings' element={
+						<div className={!navVisible ? "page" : "page page-with-navbar"}>
+							<h1>Settings</h1>
+						</div>
+					}/>
+
+					<Route path='/*' element={
+						<div className={!navVisible ? "page": "page page-with-navbar"}>
+							<Match />
+						</div>
+					}  />
+				</Routes>
+			</div>
+		</BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Match/Match.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Match = () => {
+    return (
+        <>
+          <h1>Kechirasiz bu so'rov bizga mavjud emas</h1>  
+        </>
+    );
+}
+
+export default Match;
